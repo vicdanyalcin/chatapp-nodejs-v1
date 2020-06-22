4c54ea9a6956c59bdd2e19db73b9cf3c7840d613
vicdanyalcin <vicdanyalcin.vyn@gmail.com>
2020-06-22 20:03:27 +0300

    css add

---
+++ app.js
@@ -0,0 +1,47 @@
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+const userid = makeid(8);
+// Create WebSocket connection.
+const socket = new WebSocket("ws://localhost:8080");
+
+// Connection opened
+socket.addEventListener("open", function (event) {
+  m = { username: userid, data: "Hello Server!" };
+  console.log(m);
+  socket.send(JSON.stringify(m));
+});
+
+// Listen for messages
+socket.addEventListener("message", function (event) {
+  console.log("Message from server ", event.data);
+  messages = document.getElementById("messages");
+  msg = document.createElement("div");
+  un = document.createElement("span");
+  un.classList.add("un");
+  jd = JSON.parse(event.data);
+  un.innerHTML = jd.username;
+  d = document.createElement("span");
+  d.classList.add("d");
+  d.innerHTML = jd.data;
+  msg.appendChild(un);
+  msg.appendChild(d);
+  messages.appendChild(msg);
+});
+
+function sendMessage() {
+  i_t_m = document.getElementById("msg_text");
+  t_m = i_t_m.value;
+  console.log(t_m);
+  m = { username: userid, data: t_m };
+  console.log(m);
+  socket.send(JSON.stringify(m));
+}
