c4724a34b3bb376d2e6a1a38aa4b4af75348ef9b
vicdanyalcin <vicdanyalcin.vyn@gmail.com>
2020-06-28 13:08:36 +0300

    colorizer added

--- app.js
+++ app.js
@@ -10,12 +10,29 @@ function makeid(length) {
 }
 
 const userid = makeid(8);
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  messages.style.color = color;
+  return color;
+}
+const usernameColor = getRandomColor();
+
 // Create WebSocket connection.
 const socket = new WebSocket("ws://localhost:8080");
 
 // Connection opened
 socket.addEventListener("open", function (event) {
-  m = { username: userid, data: "Hello Server!", time: new Date() };
+  m = {
+    username: userid,
+    data: "Hello Server!",
+    time: new Date(),
+    color: usernameColor,
+  };
   console.log(m);
   socket.send(JSON.stringify(m));
 });
@@ -31,13 +48,13 @@ socket.addEventListener("message", function (event) {
   un.classList.add("un");
   jd = JSON.parse(event.data);
   un.innerHTML = jd.username;
+  // add style to username
   d = document.createElement("span");
   d.classList.add("d");
   d.innerHTML = jd.data;
   date.innerHTML = jd.time;
   msg.classList.add("message");
   msg.appendChild(date);
-
   msg.appendChild(un);
   msg.appendChild(d);
   messages.appendChild(msg);
@@ -47,8 +64,16 @@ function sendMessage() {
   i_t_m = document.getElementById("msg_text");
   t_m = i_t_m.value;
   console.log(t_m);
-  m = { username: userid, data: t_m, time: new Date() };
+  m = {
+    username: userid,
+    data: t_m,
+    time: new Date(),
+    color: usernameColor,
+  };
   console.log(m);
+
   socket.send(JSON.stringify(m));
+  if (!t_m) return;
+
   document.getElementById("msg_text").value = "";
 }
